8bd6b3e06727118d6adaa5b28ccaa3a1918c4c05
fendishatech <liyukibir@gmail.com>
2023-02-07 07:18:00 -0800

    jobs detail

--- src/views/public/jobs/JobDetail.jsx
+++ src/views/public/jobs/JobDetail.jsx
@@ -1,7 +1,118 @@
-import React from "react";
-
+import { GoCalendar } from "react-icons/go";
+import { TfiAlarmClock, TfiMapAlt } from "react-icons/tfi";
+import { SlBriefcase } from "react-icons/sl";
 const JobDetail = () => {
-  return <div>JobDetail</div>;
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-8">
+          <h2>Job Overview</h2>
+          <JobDescription />
+        </div>
+        <div className="col-md-4">
+          <h2>Job Overview</h2>
+          <JobOverview />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JobDetail;
+
+const JobDescription = () => {
+  return (
+    <div className="p-2">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia quos
+        sequi voluptate cumque aliquam quam alias odit eaque ducimus dicta
+        explicabo, esse obcaecati quod laudantium soluta magni expedita fugit
+        distinctio! Cumque sequi quaerat iure pariatur beatae dolores, possimus
+        laboriosam cupiditate non incidunt tempora inventore ducimus quae
+        reprehenderit ipsum earum iusto?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia quos
+        sequi voluptate cumque aliquam quam alias odit eaque ducimus dicta
+        explicabo, esse obcaecati quod laudantium soluta magni expedita fugit
+        distinctio! Cumque sequi quaerat iure pariatur beatae dolores, possimus
+        laboriosam cupiditate non incidunt tempora inventore ducimus quae
+        reprehenderit ipsum earum iusto?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia quos
+        sequi voluptate cumque aliquam quam alias odit eaque ducimus dicta
+        explicabo, esse obcaecati quod laudantium soluta magni expedita fugit
+        distinctio! Cumque sequi quaerat iure pariatur beatae dolores, possimus
+        laboriosam cupiditate non incidunt tempora inventore ducimus quae
+        reprehenderit ipsum earum iusto?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia quos
+        sequi voluptate cumque aliquam quam alias odit eaque ducimus dicta
+        explicabo, esse obcaecati quod laudantium soluta magni expedita fugit
+        distinctio! Cumque sequi quaerat iure pariatur beatae dolores, possimus
+        laboriosam cupiditate non incidunt tempora inventore ducimus quae
+        reprehenderit ipsum earum iusto?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia quos
+        sequi voluptate cumque aliquam quam alias odit eaque ducimus dicta
+        explicabo, esse obcaecati quod laudantium soluta magni expedita fugit
+        distinctio! Cumque sequi quaerat iure pariatur beatae dolores, possimus
+        laboriosam cupiditate non incidunt tempora inventore ducimus quae
+        reprehenderit ipsum earum iusto?
+      </p>
+    </div>
+  );
+};
+
+const JobOverview = () => {
+  return (
+    <div className="bg-brand p-2">
+      <div className="d-flex flex-column">
+        <OverView
+          title="Date Posted"
+          text="2 days ago"
+          icon={<GoCalendar size={32} color={"#fff"} />}
+        />
+        <OverView
+          title="Deadline"
+          text="in 4 days"
+          icon={<TfiAlarmClock size={32} color={"#fff"} />}
+        />
+        <OverView
+          title="Location"
+          text="Addis Ababa"
+          icon={<TfiMapAlt size={32} color={"#fff"} />}
+        />
+        <OverView
+          title="Job Title"
+          text="Job title"
+          icon={<SlBriefcase size={32} color={"#fff"} />}
+        />
+        <div className="container">
+          <div className="row">
+            <button className="apply-btn">Apply Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const OverView = ({ title, icon, text }) => {
+  return (
+    <div className="d-flex p-2 gap-2">
+      <div className="bg-danger p-2 d-flex align-items-center">{icon}</div>
+      <div className="p-2">
+        <h6>{title}:</h6>
+        <p className="p-0">{text}</p>
+      </div>
+    </div>
+  );
+};
--- src/views/public/jobs/Jobs.jsx
+++ src/views/public/jobs/Jobs.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Logo } from "../../../assets/Images";
 
 const Jobs = () => {
-  return <div>Jobs</div>;
+  return (
+    <div>
+      <div className="section-title mt-4">
+        <span>We are Hiring!</span>
+        <h2>We are Hiring!</h2>
+        <p>We are looking for qualified talent in the following job posts</p>
+      </div>
+      <JobCard />
+      <JobCard />
+    </div>
+  );
 };
 
 export default Jobs;
+
+const JobCard = () => {
+  return (
+    <div className="container job-card-shadow p-4">
+      <div className="row">
+        <div className="col-md-2">
+          <img className="img-fluid" src={Logo} alt="" />
+        </div>
+        <div className="col-md-7">
+          <div className="d-flex flex-column">
+            <h2>Job Title</h2>
+            <div className="d-flex justify-content-between">
+              <p>Zerihun Associates</p>
+              <p>Location</p>
+              <p>Deadline</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 d-flex flex-column gap-3">
+          <button className="btn btn-outline-success px-5 rounded-pill">
+            Full Time
+          </button>
+          <Link
+            to="/jobs/2"
+            className="btn btn-outline-success px-5 rounded-pill"
+          >
+            See Details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
